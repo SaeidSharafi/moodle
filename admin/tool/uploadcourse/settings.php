03a1b2de98e2c9785bef21d519856950d6a5b0f7
saeid sharafi <saeed_sharafi12@yahoo.com>
2022-11-01 19:31:43 +0330

    allow user with course create permission to upload courses with csv

--- admin/tool/uploadcourse/settings.php
+++ admin/tool/uploadcourse/settings.php
@@ -24,7 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-if ($hassiteconfig) {
-    $ADMIN->add('courses', new admin_externalpage('tooluploadcourse',
-        get_string('uploadcourses', 'tool_uploadcourse'), "$CFG->wwwroot/$CFG->admin/tool/uploadcourse/index.php"));
-}
+//if ($hassiteconfig) {
+$ADMIN->add('courses', new admin_externalpage('tooluploadcourse',
+    get_string('uploadcourses', 'tool_uploadcourse'),
+    "$CFG->wwwroot/$CFG->admin/tool/uploadcourse/index.php",
+    'moodle/course:create'));
+//}
