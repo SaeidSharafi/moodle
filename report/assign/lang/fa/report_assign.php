e764bcc5bdd1457f2ecc3d45467ea6185512170e
saeid sharafi <saeed_sharafi12@yahoo.com>
2023-03-08 17:19:16 +0330

    add assignment report

---
+++ report/assign/lang/fa/report_assign.php
@@ -0,0 +1,91 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings
+ *
+ * @package    report
+ * @subpackage assign
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addextension'] = 'Add extension';
+$string['allocatedmarker'] = 'Marker';
+$string['allocationon'] = 'Allocation enabled';
+$string['annotatedpdfs'] = 'Annotated PDFs';
+$string['anonymous'] = 'Anonymous';
+$string['assign:export'] = 'Export user lists to Excel';
+$string['assign:view'] = 'View course assignment submission report';
+$string['assign:shownames'] = 'All (non-anonymised) view of submitters';
+$string['anonymousassignments'] = 'Assignments';
+$string['assignmentname'] = 'Assignment name';
+$string['blindmarkingon'] = 'Blind marking on';
+$string['byuser'] = 'By (user)';
+$string['created'] = 'Created';
+$string['duedate'] = 'Due date';
+$string['dumpfiles'] = 'Dump files';
+$string['dumpformhelp'] = 'Select the items you want in your export (default is everything)';
+$string['email'] = 'پست الکترونیک';
+$string['eventassignmentviewed'] = 'Assignment report single viewed';
+$string['eventdump'] = 'فایل های تکلیف فشرده شدند';
+$string['eventexport'] = 'تکلیف صادر شد';
+$string['eventoffline'] = 'نمره دهی آفلاین تکالیف';
+$string['eventreportviewed'] = 'گزارش نکلیف مشاهده شد';
+$string['export'] = 'صدور';
+$string['exportall'] = 'صدور همه';
+$string['extension'] = 'تمدید';
+$string['extensionpopuptitle'] = 'Set extension';
+$string['extensionhelp'] = 'Extend/add extension to due date for "{$a}" in full days';
+$string['feedback'] = 'Feedback';
+$string['feedbackcomments'] = 'Feedback comments';
+$string['feedbackfiles'] = 'Feedback files';
+$string['grade'] = 'Grade';
+$string['grader'] = 'Grader';
+$string['group'] = 'Group';
+$string['groupsubmission'] = 'Group submission';
+$string['log'] = 'Log';
+$string['name'] = 'Name';
+$string['noassignments'] = 'There are no assignments in this course';
+$string['nocapability'] = 'You do not have the required capabiltities to view this report';
+$string['nologdata'] = 'No log data found for this submission';
+$string['notallowed'] = 'You do not have permission to view the selected activity';
+$string['offline'] = 'Offline';
+$string['offlineworkbook'] = 'Offline grading export';
+$string['participantno'] = 'شماره شرکت کننده';
+$string['plagiarism'] = 'Plagiarism';
+$string['pluginname'] = 'Assignment submission report';
+$string['privacy:metadata'] = 'The Assignment submission report only shows data stored in other locations.';
+$string['profilefields'] = 'Profile fields';
+$string['profilefields_desc'] = 'Include these additional profile fields in the report';
+$string['released'] = 'تحویل داده شده';
+$string['revealidentities'] = 'Identities revealed';
+$string['selectassignment'] = 'Select assignment to view submission report';
+$string['status'] = 'Status';
+$string['statusdraft'] = 'draft';
+$string['statusnew'] = 'new';
+$string['statusreopened'] = 'reopened';
+$string['statussubmitted'] = 'submitted';
+$string['submissions'] = 'Submissions';
+$string['submitdate'] = 'Submit date';
+$string['submitted'] = 'Submitted';
+$string['title'] = 'Title';
+$string['turnitin'] = 'Turnitin';
+$string['turnitinenabled'] = 'Turnitin enabled';
+$string['urkund'] = 'Urkund';
+$string['urkundenabled'] = 'Urkund enabled';
+$string['workbook'] = 'Assignment submission report export';
+$string['workflow'] = 'Workflow';
+$string['workflowon'] = 'Workflow enabled';
