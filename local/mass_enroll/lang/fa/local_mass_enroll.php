f4e548e4c71c4d93f26326ca3e38220f8c691e78
saeed <saeed_sharafi12@yahoo.com>
2023-12-31 13:31:23 +0330

    add mass enrol & remui course format

---
+++ local/mass_enroll/lang/fa/local_mass_enroll.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file for local_mass_enroll, EN-US
+ *
+ * File         local_mass_enroll.php
+ * Encoding     UTF-8
+ *
+ * @package     local_mass_enroll
+ *
+ * @copyright   1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @copyright   2012 onwards Patrick Pollet
+ * @copyright   2015 onwards R.J. van Dongen <rogier@sebsoft.nl>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'Mass enrollments';
+
+// Capabilities name required Moodle 2.3.
+$string['mass_enroll:enrol'] = 'ثبت نام کاربران در دوره توسط فایل CSV';
+$string['mass_enroll:unenrol'] = 'لغو ثبت نام کاربران از دوره توسط فایل CSV';
+
+$string['mass_enroll'] = 'ثبت نام گروهی';
+$string['mass_unenroll'] = 'لغو ثبت نام گروهی';
+
+$string['enroll'] = 'ثبت نام در دوره';
+$string['unenroll'] = 'لغو ثبت نام از دوره';
+
+$string['mailreport'] = 'گزارش را برایم ایمیل کن';
+$string['creategroups'] = 'ایجاد گروه در صورت نیاز';
+$string['creategroupings'] = 'گروه بندی  در صورت نیاز';
+$string['firstcolumn'] = 'ستون اول شامل';
+$string['roleassign'] = 'نقش منتسب';
+$string['idnumber'] = 'Id number';
+$string['username'] = 'نام کاربری';
+$string['mail_enrolment_subject'] = 'ثبت نام گروهی در {$a}';
+$string['mail_unenrolment_subject'] = 'لغو ثبت نام گروهی از {$a}';
+$string['email_sent'] = 'email sent to {$a}';
+$string['im:using_role'] = 'ثبت نام کاربر : {$a} ';
+$string['im:opening_file'] = 'در حال باز کردن فایل : {$a} ';
+$string['im:user_unknown'] = '{$a} ناشناس - نادیده گرفتن این خط';
+$string['im:already_in'] = 'در حال حاضر ثبت نام شده است {$a}';
+$string['im:enrolled_ok'] = '{$a} ثبت نام شد ';
+$string['im:error_in'] = 'خطا در ثبت نام {$a}';
+$string['im:not_in'] = '{$a} ثبت نام نشد ';
+$string['im:unenrolled_ok'] = 'ثبت نام {$a} لغو شد';
+$string['im:error_out'] = 'خطا در لغو ثبت نام {$a}';
+
+$string['massenrollsettings'] = 'تنظیمات ثبت نام گروهی';
