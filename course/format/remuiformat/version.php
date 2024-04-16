0ac4ca56890f512d26d1f1c61a23c8055804b613
saeed <saeed_sharafi12@yahoo.com>
2024-04-16 15:24:53 +0330

    change course format style (make it smaller)

--- course/format/remuiformat/version.php
+++ course/format/remuiformat/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023101700.1;                    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023101700.2;                    // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release   = '4.1.4';
 $plugin->requires  = 2022041900;                    // Requires this Moodle version (Moodle V4.0).
 $plugin->maturity  = MATURITY_STABLE;
