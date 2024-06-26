31e0351591cbaf63b4e0fc667fa8f9f1c1a091a5
saeed <saeed_sharafi12@yahoo.com>
2024-06-26 09:47:56 +0330

    add auto block adder plugin

---
+++ local/blockmanager/version.php
@@ -0,0 +1,7 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2023050705;
+$plugin->release   = '0.1';
+$plugin->requires = 2019111200;
+$plugin->component = 'local_blockmanager';
