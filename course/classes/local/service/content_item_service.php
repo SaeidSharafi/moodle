16fd618d58638a9bbacb65a461cf250104a4c272
saeed <saeed_sharafi12@yahoo.com>
2024-07-10 16:40:01 +0330

    filter by section for vums cours format

--- course/classes/local/service/content_item_service.php
+++ course/classes/local/service/content_item_service.php
@@ -243,27 +243,8 @@ class content_item_service {
             return [];
         }
 
-        $filterbysection =[
-            '1' => [
-                'adobeconnect'
-            ],
-            '2' => [
-                'file',
-            ],
-            '3' => [
-                'assign',
-                'assignment'
-            ],
-            '4' => [
-                'book'
-            ],
-            '5' => [
-                'game'
-            ],
-            '6' => [
-                'page'
-            ],
-        ];
+
+
         // Get all the visible content items.
         $allcontentitems = $this->repository->find_all_for_course($course, $user);
 
@@ -295,12 +276,23 @@ class content_item_service {
             // Check the parent module access for the user.
             return course_allowed_module($course, explode('_', $parents[$contentitem->get_component_name()])[1], $user);
         });
-        if ($linkparams && isset($linkparams['secid']) && $linkparams['secid'] > 0 && $linkparams['secid'] < 7) {
-            $availablecontentitems = array_filter($allcontentitems,
-                function ($contentitem) use ($course, $user, $parents, $filterbysection,$linkparams) {
-                    // Check the parent module access for the user.
-                    return in_array(explode('_', $parents[$contentitem->get_component_name()])[1], $filterbysection[$linkparams['secid']]);
-                });
+        if ($course->format === 'vums'){
+            $filterbysection =[
+                '1' => explode(',',get_config('format_vums', 'section1activitytypes')),
+                '2' => explode(',',get_config('format_vums', 'section2activitytypes')),
+                '3' => explode(',',get_config('format_vums', 'section3activitytypes')),
+                '4' => explode(',',get_config('format_vums', 'section4activitytypes')),
+                '5' => explode(',',get_config('format_vums', 'section5activitytypes')),
+                '6' => explode(',',get_config('format_vums', 'section6activitytypes')),
+            ];
+
+            if ($linkparams && isset($linkparams['secid']) && $linkparams['secid'] > 0 && $linkparams['secid'] < 7) {
+                $availablecontentitems = array_filter($allcontentitems,
+                    function ($contentitem) use ($course, $user, $parents, $filterbysection,$linkparams) {
+                        // Check the parent module access for the user.
+                        return in_array(explode('_', $parents[$contentitem->get_component_name()])[1], $filterbysection[$linkparams['secid']]);
+                    });
+            }
         }
         // Add the link params to the link, if any have been provided.
         if (!empty($linkparams)) {
